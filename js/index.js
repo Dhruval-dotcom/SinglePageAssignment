1ee51b07347bf20046de7c69dbd993e9efae428a
dhruval <dhruvaldevaliya@gmail.com>
2023-03-17 15:35:25 +0530

    optimized javascript

--- js/index.js
+++ js/index.js
@@ -1,17 +1,11 @@
-var curr="home";
+var currentpage="home";
 function func(p) {
   var elList = document.getElementById(curr).classList.remove('active');
   document.getElementById(p).classList.add("active");
-  curr=p;
+  currentpage=p;
   const req = new window.XMLHttpRequest();
   req.open("GET", p + ".html", false);
   req.send();
   document.getElementById("loadcontent").innerHTML = req.response;
 }
 func("home");
-
-$(document).click(function (event) {
-  var text = $(event.target.id);
-  text.classList.add("active");
-  console.log(text);
-});
